fdc3f57a0efc76946bf91e7a759ec16fe702a520
admin <admin@192.168.0.2>
2019-08-03 13:26:53 +0530

    OrangeHrm500_Upload 3/8/19_1:30pm

---
+++ OrangeHrm500/src/main/java/OrangeHrm500/OrangeHrm500/App.java
@@ -0,0 +1,13 @@
+package OrangeHrm500.OrangeHrm500;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
